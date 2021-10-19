5daf4d1ba050d135b640993f6eebbfb95c2b81a1
Oleh <crael3r2013@gmail.com>
2021-10-19 20:22:52 +0300

    Initial commit

---
+++ Bus Station/Bus Station/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bus_Station
+{   interface BusStation
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bus Station");
+
+        }
+    }
+    class Station
+    {
+
+    }
+}
